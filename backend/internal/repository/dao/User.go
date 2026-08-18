480e6ee8de4d5bd75d9af9875d877a0a8271b582
codeLambQ <codeLamb@163.com>
2026-08-18 22:54:15 +0800

    增加登录业务逻辑，修改注册逻辑代码漏洞

--- backend/internal/repository/dao/User.go
+++ backend/internal/repository/dao/User.go
@@ -2,9 +2,9 @@ package dao
 
 // User 入库数据库的结构体
 type User struct {
-	ID       int64  `gorm:"primaryKey;autoIncrement"`
-	Email    string `gorm:"unique"`
-	Password string
-	CreateAt int64
-	UpdateAt int64
+	ID        int64  `gorm:"primaryKey;autoIncrement"`
+	Email     string `gorm:"unique"`
+	Password  string
+	CreatedAt int64
+	UpdatedAt int64
 }
